a1e5948df2e4feb7c0d852e11d76703d9502f6ec
Arvind Pandey <114512882+arvindpandeysrm@users.noreply.github.com>
2025-12-16 11:20:39 +0530

    UC1: Welcome message and program start

---
+++ uc1.py
@@ -0,0 +1,9 @@
+class AddressBookSystem:
+    def __init__(self):
+        print("Welcome to Address Book Program")
+
+def main():
+    system = AddressBookSystem()
+
+if __name__ == "__main__":
+    main()
